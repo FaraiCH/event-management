a14ecee741d9a60d8a4bc1ab782da898d286ee31
FaraiCH <faraichaka@gmail.com>
2024-04-28 14:54:45 +0200

    yes

--- database/migrations/2024_04_26_111357_create_attendees_table.php
+++ database/migrations/2024_04_26_111357_create_attendees_table.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::create('attendees', function (Blueprint $table) {
             $table->id();
+            $table->foreignIdFor(\App\Models\User::class);
+            $table->foreignIdFor(\App\Models\Event::class);
             $table->timestamps();
         });
     }
---
+++ routes/api.php
@@ -0,0 +1,12 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+
+Route::apiResource('events', \App\Http\Controllers\EventController::class);
+Route::apiResource('events.attendees', \App\Http\Controllers\AttendeeController::class)->scoped(['attendee' => 'event']);
